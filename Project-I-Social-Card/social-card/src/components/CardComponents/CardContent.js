1124800d71d9c38e3892feb91e8c31582cfa980c
LVWood <leslee.wood@gmail.com>
2019-04-22 19:49:59 -0500

    social card components built. mostly finished styling

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+function CardContent() {
+    return (
+        <div className="card-content" href="http://reactjs.org">
+            <h1>Get Started with React</h1>
+            <p>React makes it painless to create interactive UIs. Design Simple views for each state in your application.</p>
+            <a href="http://reactjs.org">reactjs.org</a>
+        </div>
+    )
+};
+
+export default CardContent;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
+
+import ImageThumbnail from './ImageThumbnail.js';
+import HeaderContent from './HeaderContent.js';
+
 import './Header.css';
+
+function HeaderContainer(){
+    return (
+        <div className="header-container">
+            <ImageThumbnail />
+            <HeaderContent />
+        </div>
+    )
+};
+
+export default HeaderContainer;
