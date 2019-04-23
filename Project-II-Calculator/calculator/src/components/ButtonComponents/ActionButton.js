2567087f9055479844fe3e0158b43b45ad4f7fb4
LVWood <leslee.wood@gmail.com>
2019-04-23 17:49:54 -0500

    all styles built out, text not showing up

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Button.css';
+
+const ActButton = <button>{}</button>;
+const OpButton = <button>{}</button>;
+
+const ActionButton = props => {
+    return (
+        <div>
+            <button className="action-button">{ActButton.props.text}</button>
+            <button className="operator-button">{OpButton.props.text}</button>
+        </div>
+    )
+    
+};
+
+export default ActionButton;
---
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/Buttons.js
@@ -0,0 +1,21 @@
+// import React from 'react';
+// import './Button.css';
+
+// // const buttons = [
+// //     <button className="action-button" index="0" >0</button>
+// //     <button className="number-button" index="1" >1</button>
+// //     <button className="number-button" index="2" >2</button>
+// //     <button className="number-button" index="3" >3</button>
+// //     <button className="number-button" index="4" >4</button>
+// //     <button className="number-button" index="5" >5</button>
+// //     <button className="number-button" index="6">6</button>
+// //     <button className="number-button" index="7">7</button>
+// //     <button className="number-button" index="8">8</button>
+// //     <button className="number-button" index="9">9</button>
+// //     <button className="operator-button" index="10">/</button>
+// //     <button className="operator-button" index="11">x</button>
+// //     <button className="operator-button" index="12">-</button>
+// //     <button className="operator-button" index="13">+</button>
+// //     <button className="operator-button" index="14">=</button>
+// //     <button className="action-button" index="15">clear</button>
+// // ];
